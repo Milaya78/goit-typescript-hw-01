dfea9dc6ae8c64969a1d92771f0680506aa84fab
Milaya78 <ela_keyt@mail.ru>
2024-11-19 19:15:10 +0100

    hw-ts-1

---
+++ src/basic/6.ts
@@ -0,0 +1,39 @@
+// const mango = {
+//   name: "Mango",
+//   age: 30,
+//   email: "john@example.com",
+//   address: {
+//     city: "New York",
+//     country: "USA",
+//   },
+// };
+
+// const poly = {
+//   name: "Mango",
+//   age: 30,
+//   email: "john@example.com",
+// };
+//Створіть інтерфейс User для типізації об'єктів, які містять такі властивості.
+//Зверніть увагу, що адреса є необов'язковою властивістю.
+interface User1 {
+  name: string;
+  age: number;
+  email: string;
+  address?: { city: string; country: string };
+}
+
+const mango: User1 = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
+  address: {
+    city: "New York",
+    country: "USA",
+  },
+};
+
+const poly: User1 = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
+};
